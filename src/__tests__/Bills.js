a4cd3e2dcd1dfe3d3b13160f1c207cb77ace6606
ACH90 <chassagne.alexandre@gmail.com>
2025-03-10 17:57:46 +0100

    Test Line 14, 19 on Bills.js ok

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -2,11 +2,13 @@
  * @jest-environment jsdom
  */
 
-import { screen, waitFor } from "@testing-library/dom";
+import { fireEvent, screen, waitFor } from "@testing-library/dom";
 import BillsUI from "../views/BillsUI.js";
+import Bills from "../containers/Bills.js";
 import { bills } from "../fixtures/bills.js";
-import { ROUTES_PATH } from "../constants/routes.js";
+import { ROUTES, ROUTES_PATH } from "../constants/routes.js";
 import { localStorageMock } from "../__mocks__/localStorage.js";
+import userEvent from "@testing-library/user-event";
 
 import router from "../app/Router.js";
 
@@ -42,6 +44,52 @@ describe("Given I am connected as an employee", () => {
       const antiChrono = (a, b) => (a < b ? 1 : -1);
       const datesSorted = [...dates].sort(antiChrono);
       expect(dates).toEqual(datesSorted);
+    });
+    //-------------------MES TESTS------------------
+    describe("When I click on the New bill button", () => {
+      test("Then I should be redirected to new bill form", () => {
+        document.body.innerHTML = BillsUI({ data: bills });
+        const onNavigate = (pathname) => {
+          document.body.innerHTML = ROUTES({ pathname });
+        };
+        const sampleBills = new Bills({
+          document,
+          onNavigate,
+          store: null,
+          localStorage: window.localStorage,
+        });
+        const handleClickNewBill = jest.fn(sampleBills.handleClickNewBill);
+        const newBillButton = screen.getByTestId("btn-new-bill");
+        newBillButton.addEventListener("click", handleClickNewBill);
+        userEvent.click(newBillButton);
+        expect(handleClickNewBill).toHaveBeenCalled();
+        expect(screen.getByText("Envoyer une note de frais")).toBeTruthy();
+      });
+    });
+
+    test("Then clicking on the eye icon should open a modal with handleClickIconEye", () => {
+      //Mock de la fonction handleClickIconEye
+      const handleClickIconEye = jest.fn();
+      //Ajout d'une icone dans le DOM
+      document.body.innerHTML = `<div data-testid="icon-eye"></div>`;
+
+      //Instanciation de Bills avec la fonction mockée
+      const bills = new Bills({
+        document,
+        onNavigate: jest.fn(),
+        store: null,
+        localStorage: window.localStorage,
+      });
+
+      //Remplacer la fonction handleClickIconEye
+      bills.handleClickIconEye = handleClickIconEye;
+
+      //Recuperation et déclenchement du clic
+      const iconEye = screen.getByTestId("icon-eye");
+      fireEvent.click(iconEye);
+
+      //Verification de l'appel de la fonction handleClickIconEye
+      expect(handleClickIconEye).toHaveBeenCalledWith(iconEye);
     });
   });
 });
